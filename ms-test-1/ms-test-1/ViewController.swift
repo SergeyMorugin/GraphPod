4137ea461d24d49cd13a94235862541fe5476e63
Sergei Morugin <cmorugin@gmail.com>
2021-06-17 14:41:11 +0300

    add blanck app project

---
+++ ms-test-1/ms-test-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ms-test-1
+//
+//  Created by Matthew on 17.06.2021.
+//  Copyright © 2021 Ostagram Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
