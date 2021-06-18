0b25ee2a97382d2d2f0e37d078050af8262704d8
Sergei Morugin <cmorugin@gmail.com>
2021-06-18 18:39:14 +0300

    add image picker

--- ms-test-1/ms-test-1/ViewController.swift
+++ ms-test-1/ms-test-1/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        resultImage.image = UIImage(named:"test4")
     }
 
     @IBAction func onRunClick(_ sender: Any) {
@@ -23,16 +24,16 @@ class ViewController: UIViewController {
         
         let startTime = CFAbsoluteTimeGetCurrent()
         // Get inital image
-        guard let processingImage = UIImage(named:"test4") else { return }
+        guard let processingImage = resultImage.image else { return }
        
         
-        //guard let smoothImage = processingImage.smoothing(sigma: 0.6) else { return }
+        guard let smoothImage = processingImage.smoothing(sigma: 0.6) else { return }
         //resultImage.image = smoothImage
         print("Gauss smooth: \(CFAbsoluteTimeGetCurrent() - startTime) s.")
-        guard let image = processingImage.toBitmapImage() else { return }
+        guard let image = smoothImage.toBitmapImage() else { return }
  
         //print(image)
-        let result = SegmentingImageAlgorithm().segmentImage(image, threshold: 200, minSize: 400)
+        let result = SegmentingImageAlgorithm().segmentImage(image, threshold: 0.2, minSize: 10)
         //print(resultImage)
        
         
@@ -40,10 +41,55 @@ class ViewController: UIViewController {
         im?.cgImage?.copy(colorSpace: processingImage.cgImage!.colorSpace!)
 
         resultImage.image = im
-        let resultText = "Found \(result!.1.roots.count) sections in \(CFAbsoluteTimeGetCurrent() - startTime) s"
+        let resultText = "Found \(result!.1.roots.count) sections in \(round((CFAbsoluteTimeGetCurrent() - startTime)*1000)/1000) s for image \(processingImage.size.width)x\(processingImage.size.height)"
         print(resultText)
         resultLabel.text = resultText
     }
     
+    @IBAction func onLoadPhotoClick(_ sender: Any) {
+        takeAPhoto()
+    }
+    
+    
+    func takeAPhoto() {
+        let imagePickerController = UIImagePickerController()
+       if UIImagePickerController.isSourceTypeAvailable(.camera) {
+           imagePickerController.sourceType = .camera
+       } else {
+           imagePickerController.sourceType = .savedPhotosAlbum
+       }
+       imagePickerController.allowsEditing = true
+       imagePickerController.delegate = self
+        UIApplication.shared.keyWindow?.rootViewController?.present(imagePickerController, animated: true)
+    }
+    
+}
+
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+   
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        let image = extractImage(from: info)
+        resultImage.image = image
+        resultLabel.text = ""
+        picker.dismiss(animated: true)
+    }
+    
+    private func extractImage(from info: [UIImagePickerController.InfoKey: Any]) -> UIImage? {
+        // Пытаемся извлечь отредактированное изображение
+        if let image = info[.editedImage] as? UIImage {
+            return image
+            // Пытаемся извлечь оригинальное
+        } else if let image = info[.originalImage] as? UIImage {
+            return image
+        } else {
+            // Если изображение не получено, возвращаем nil
+            return nil
+        }
+    }
 }
 
