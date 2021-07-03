5f94578e039f920daef4d66094a04b0fefa8cd8a
Sergei Morugin <cmorugin@gmail.com>
2021-07-03 18:32:44 +0300

    create a new files for project

---
+++ Example/GraphPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GraphPod
+//
+//  Created by segiomars@gmail.com on 07/03/2021.
+//  Copyright (c) 2021 segiomars@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
